c88cd864a44c1c273b5ca95bde5a33cf3f0f397a
iheb-harchi <ihebharchi@outlook.de>
2020-11-11 18:12:24 +0100

    init project

---
+++ CAR_RENTAL/src/main/java/com/car/rental/CAR_RENTAL/App.java
@@ -0,0 +1,13 @@
+package com.car.rental.CAR_RENTAL;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
